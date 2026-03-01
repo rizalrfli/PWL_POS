27e6a71cedd31c8602053f8b3910368c158bcf6c
afrizalrfli <tkj1211afrizalrafli@smkn1glagah.sch.id>
2026-03-01 19:55:40 +0700

    fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,9 +12,12 @@ class UserController extends Controller
     {
         // 1. Update data user dengan Eloquent Model
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
-        UserModel::where('username', 'customer-1')->update($data); // update data user
+        UserModel::create($data);
 
         // 2. Coba akses model UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_user
