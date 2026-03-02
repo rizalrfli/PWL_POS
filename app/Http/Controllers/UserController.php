c61fceb5b3db5f7ad91145ce0070f0be65ac6bfb
afrizalrfli <tkj1211afrizalrafli@smkn1glagah.sch.id>
2026-03-02 11:15:21 +0700

    Praktikum 2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,9 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstWhere('level_id', 1);
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
