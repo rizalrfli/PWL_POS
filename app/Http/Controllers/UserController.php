7fd0d8b37072b22416e5ffccc05f102ddd62b929
afrizalrfli <tkj1211afrizalrafli@smkn1glagah.sch.id>
2026-03-04 08:27:54 +0700

    Praktikum 4 (JS 5)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -22,8 +22,9 @@ class UserController extends Controller
         ];
 
         $activeMenu = 'user'; // set menu yang sedang aktif
+        $level = LevelModel::all();
 
-        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
     }
 
     // Ambil data user dalam bentuk json untuk datatables 
@@ -31,6 +32,10 @@ class UserController extends Controller
     {
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
             ->with('level');
+
+        if ($request->level_id) {
+            $users->where('level_id', $request->level_id);
+        }
 
         return DataTables::of($users)
             // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex) 
